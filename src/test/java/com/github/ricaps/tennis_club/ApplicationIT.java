d002160a11c505d108a0585263acc0082a6078ad
Martin Prochazka <pro.nitram@gmail.com>
2025-11-01 15:37:22 +0100

    Setup Lombok & MapStruct & Spring JavaFormat

--- src/test/java/com/github/ricaps/tennis_club/ApplicationIT.java
+++ src/test/java/com/github/ricaps/tennis_club/ApplicationIT.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApplicationIT {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
