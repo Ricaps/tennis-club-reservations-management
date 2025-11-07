4aa7be533c8511ccf60fc9fbf563afc546f1f341
Martin Prochazka <pro.nitram@gmail.com>
2025-11-07 15:41:30 +0100

    Change overlap calculation

--- src/test/java/com/github/ricaps/tennis_club/peristence/dao/ReservationDaoImplTest.java
+++ src/test/java/com/github/ricaps/tennis_club/peristence/dao/ReservationDaoImplTest.java
@@ -1,6 +1,8 @@
 package com.github.ricaps.tennis_club.peristence.dao;
 
+import com.github.ricaps.tennis_club.exception.ValueIsMissingException;
 import com.github.ricaps.tennis_club.peristence.dao.definition.CrudDao;
+import com.github.ricaps.tennis_club.peristence.dao.definition.ReservationDao;
 import com.github.ricaps.tennis_club.peristence.entity.Court;
 import com.github.ricaps.tennis_club.peristence.entity.MoneyAmount;
 import com.github.ricaps.tennis_club.peristence.entity.Reservation;
@@ -10,22 +12,34 @@ import com.github.ricaps.tennis_club.test_utils.CourtTestData;
 import com.github.ricaps.tennis_club.test_utils.ReservationTestData;
 import com.github.ricaps.tennis_club.test_utils.SurfaceTestData;
 import com.github.ricaps.tennis_club.test_utils.UserTestData;
+import com.github.ricaps.tennis_club.utils.UUIDUtils;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 
 import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.Currency;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Import(ReservationDaoImpl.class)
 class ReservationDaoImplTest extends AbstractDaoTest<Reservation> {
 
 	@Autowired
 	EntityManager entityManager;
+
+	@Autowired
+	ReservationDao reservationDao;
 
 	private Court court;
 
@@ -68,6 +82,71 @@ class ReservationDaoImplTest extends AbstractDaoTest<Reservation> {
 	protected Reservation updateEntity(Reservation entity) {
 		entity.setTotalPrice(new MoneyAmount(new BigDecimal("25.5"), Currency.getInstance("CZK")));
 		return entity;
+	}
+
+	private static Stream<Arguments> provideIntervalTestArguments() {
+		Reservation reservation = ReservationTestData.entity(null, null);
+
+		return Stream.of(
+				Arguments.of(reservation, reservation.getFromTime().minusHours(1),
+						reservation.getFromTime().minusMinutes(1), false, "No overlap under interval"),
+				Arguments.of(reservation, reservation.getToTime().plusMinutes(1), reservation.getToTime().plusHours(1),
+						false, "No overlap above interval"),
+				Arguments.of(reservation, reservation.getFromTime().minusHours(1), reservation.getFromTime(), true,
+						"Overlap at interval beginning"),
+				Arguments.of(reservation, reservation.getToTime(), reservation.getToTime().plusHours(1), true,
+						"Overlap at interval end"),
+				Arguments.of(reservation, reservation.getFromTime().minusMinutes(30),
+						reservation.getFromTime().plusMinutes(30), true, "Overlap over beginning"),
+				Arguments.of(reservation, reservation.getToTime().minusMinutes(30),
+						reservation.getToTime().plusMinutes(30), true, "Overlap over end"),
+				Arguments.of(reservation, reservation.getFromTime().minusMinutes(30),
+						reservation.getToTime().plusMinutes(30), true, "Full overlap of interval"),
+				Arguments.of(reservation, reservation.getFromTime().plusMinutes(1),
+						reservation.getToTime().minusMinutes(1), true, "Inside interval"),
+				Arguments.of(reservation, reservation.getFromTime(), reservation.getToTime(), true,
+						"Exact same interval"));
+	}
+
+	@Test
+	void getReservationsAtTimeFrame_fromNull_throwsException() {
+		OffsetDateTime offsetDateTime = OffsetDateTime.now();
+		assertThatThrownBy(() -> reservationDao.getReservationsAtTimeFrame(null, offsetDateTime, UUIDUtils.generate()))
+			.isInstanceOf(ValueIsMissingException.class);
+	}
+
+	@Test
+	void getReservationsAtTimeFrame_toNull_throwsException() {
+		OffsetDateTime offsetDateTime = OffsetDateTime.now();
+		assertThatThrownBy(() -> reservationDao.getReservationsAtTimeFrame(offsetDateTime, null, UUIDUtils.generate()))
+			.isInstanceOf(ValueIsMissingException.class);
+	}
+
+	@Test
+	void getReservationsAtTimeFrame_uuidNull_throwsException() {
+		OffsetDateTime offsetDateTime = OffsetDateTime.now();
+		assertThatThrownBy(() -> reservationDao.getReservationsAtTimeFrame(offsetDateTime, offsetDateTime, null))
+			.isInstanceOf(ValueIsMissingException.class);
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideIntervalTestArguments")
+	void getReservationsAtTimeFrame_exactSameInterval_reservationReturned(Reservation reservation, OffsetDateTime from,
+			OffsetDateTime to, boolean expectValue, String message) {
+		reservation.setUser(user);
+		reservation.setCourt(court);
+		reservationDao.save(reservation);
+
+		List<Reservation> reservationList = reservationDao.getReservationsAtTimeFrame(from, to,
+				reservation.getCourt().getUid());
+
+		if (expectValue) {
+			assertThat(reservationList).withFailMessage(message).hasSize(1);
+			assertThat(reservationList.getFirst()).isEqualTo(reservation);
+		}
+		else {
+			assertThat(reservationList).withFailMessage(message).isEmpty();
+		}
 	}
 
 }
